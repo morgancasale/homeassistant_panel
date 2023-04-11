812a8b31776e24f2194b43f6190d0365e03dcadc
morgancasale <morgan.casale@gmail.com>
2023-04-11 16:28:07 +0200

    Implemented house settings

--- main-page.js
+++ main-page.js
@@ -8,6 +8,7 @@ import {
 import "https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js";
 
 import "./socket-settings.js";
+import "./house-settings.js"
 
 class MainPage extends LitElement {
     static get properties() {
@@ -16,29 +17,46 @@ class MainPage extends LitElement {
             narrow: { type: Boolean },
             route: { type: Object },
             panel: { type: Object },
-            data: { type: Object },
-            loadSettings: { type: Boolean }
+            devicesData: { type: Object },
+            houseData: { type: Object },
+            loadSocketSettings: { type: Boolean }
         };
     }
     
     constructor(){
         super();
-        this.data = {};
+        this.devicesData = {};
+        this.houseData = {};
         this.errState = false;
     }
 
-    hide_socket(){
+    async hide_settings(){
         this.fetchData();
         this.shadowRoot.getElementById("container").style.justifyContent = "";
-        this.shadowRoot.getElementById("socket-settings").style.display = "none";
+        
+        var house_el = this.shadowRoot.getElementById("house-settings");
+        if(house_el != null){
+            house_el.style.display = "none";
+        }
+        var socket_el = this.shadowRoot.getElementById("socket-settings");
+        if(socket_el != null){
+            socket_el.style.display = "none";
+        }
+
         this.shadowRoot.getElementById("btn_cont").style.display = "none";
 
         this.shadowRoot.getElementById("main-page").style.display = "flex";
 
-        this.loadSettings = false;
+        this.loadSocketSettings = false;
+        this.loadHouseSettings = false;
+
+        this.shadowRoot.getElementById("gen_settings_button").style.display = "block";
+
+        if(socket_el != null){ this.resetDelState(); }
         this.resetSavedState();
+
         this.reRender();
-        this.resetDelState();
+        await this.requestUpdate();
     }
 
     async reRender(){
@@ -47,20 +65,34 @@ class MainPage extends LitElement {
     }
 
     getSocketData(socket_name){
-        return this.data.find(el => el["deviceName"] == socket_name);
+        return this.devicesData.find(el => el["deviceName"] == socket_name);
     }
 
-    async show_settings(socket) {
+    async show_socket_settings(socket) {
         this.socketData = this.getSocketData(socket);
-        this.extData = this.socketData;
-        this.loadSettings = true;
+        this.extDeviceData = this.socketData;
+        this.loadSocketSettings = true;
         await this.reRender();
 
         this.shadowRoot.getElementById("socket-settings").setData(this.socketData);
         this.shadowRoot.getElementById("main-page").style.display = "none";
+        this.shadowRoot.getElementById("gen_settings_button").style.display = "none";
 
         this.shadowRoot.getElementById("container").style.justifyContent = "center";
         this.shadowRoot.getElementById("socket-settings").style.display = "flex";
+        this.shadowRoot.getElementById("btn_cont").style.display = "flex";
+    }
+
+    async show_house_settings(){
+        this.loadHouseSettings = true;
+        await this.requestUpdate();
+
+        //this.shadowRoot.getElementById("house-settings").setData(this.houseData);
+        this.shadowRoot.getElementById("main-page").style.display = "none";
+        this.shadowRoot.getElementById("gen_settings_button").style.display = "none";
+
+        this.shadowRoot.getElementById("container").style.justifyContent = "center";
+        this.shadowRoot.getElementById("house-settings").style.display = "flex";
         this.shadowRoot.getElementById("btn_cont").style.display = "flex";
     }
 
@@ -136,42 +168,78 @@ class MainPage extends LitElement {
         text.style.color = "";
     }
 
-    save(){
-        try{
-            this.socketData = this.shadowRoot.getElementById("socket-settings").save();
-            this.socketData.deviceID = this.extData.deviceID;
+    save_socket_settings(){
+        this.socketData = this.shadowRoot.getElementById("socket-settings").save();
+        this.socketData.deviceID = this.extDeviceData.deviceID;
 
-            var cond = (this.socketData.deviceName != this.extData.deviceName) | (this.socketData.deviceName == null);
-            this.socketData.deviceName = (cond) ? this.socketData.deviceName : this.extData.deviceName;
+        var cond = (this.socketData.deviceName != this.extDeviceData.deviceName) | (this.socketData.deviceName == null);
+        this.socketData.deviceName = (cond) ? this.socketData.deviceName : this.extDeviceData.deviceName;
 
-            var url = "http://192.168.2.145:8099/setDeviceSettings";
-            var request = {
-                method: "PUT", // *GET, POST, PUT, DELETE, etc.
-                mode: "cors", // no-cors, *cors, same-origin
-                cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-                credentials: "same-origin", // include, *same-origin, omit
-                headers: {
-                    "Content-Type": "application/json",
-                    // 'Content-Type': 'application/x-www-form-urlencoded',
-                }, // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-                body: JSON.stringify([this.socketData])// body data type must match "Content-Type" header
-            };
-            
-            if(!this.errState){
-                fetch(url, request)
-                .then((response) => {
-                    this.SavedState(response.ok);
-                })
-                .catch(error => {
-                    throw new Error("An error occured while performing PUT: \n\t" + error.message);
-                });
-            }
-        } catch(e){
-            alert("Error in the main page: \n\t" + e.message);
+        var url = "http://192.168.2.145:8099/setDeviceSettings";
+        var request = {
+            method: "PUT", // *GET, POST, PUT, DELETE, etc.
+            mode: "cors", // no-cors, *cors, same-origin
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "same-origin", // include, *same-origin, omit
+            headers: {
+                "Content-Type": "application/json",
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            }, // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body: JSON.stringify([this.socketData])
+        };
+        
+        if(!this.errState){
+            fetch(url, request)
+            .then((response) => {
+                this.SavedState(response.ok);
+            })
+            .catch(error => {
+                throw new Error("An error occured while performing PUT request: \n\t" + error.message);
+            });
         }
     }
 
-    fetchData(){ // Fetch sockets settings
+    save_house_settings(){
+        var newHouseData = this.shadowRoot.getElementById("house-settings").save();
+        Object.assign(newHouseData, { houseID : this.houseData.houseID })
+
+        var url = "http://192.168.2.145:8099/setHouseSettings";
+        var request = {
+            method: "PUT", // *GET, POST, PUT, DELETE, etc.
+            mode: "cors", // no-cors, *cors, same-origin
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "same-origin", // include, *same-origin, omit
+            headers: {
+                "Content-Type": "application/json",
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            }, // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body: JSON.stringify([newHouseData])
+        };
+        
+        if(!this.errState){
+            fetch(url, request)
+            .then((response) => {
+                this.SavedState(response.ok);
+            })
+            .catch(error => {
+                throw new Error("An error occured while performing PUT request: \n\t" + error.message);
+            });
+        }
+    }
+
+    save(){
+        try{
+            if(this.loadHouseSettings){
+                this.save_house_settings();
+            } else if(this.loadSocketSettings){
+                this.save_socket_settings();
+            }
+        } catch(e){
+            alert("An error occured while saving settings: \n\t" + e.message);
+        }
+    }
+
+    fetchSocketSettings(){
         var url = "http://192.168.2.145:8099/getInfo?";
         var params = {
             table : "DeviceSettings",
@@ -182,25 +250,55 @@ class MainPage extends LitElement {
 
         fetch(url + params)
         .then((response) => response.json())
-        .then((data) => {
-            this.data = data;
+        .then((devicesData) => {
+            this.devicesData = devicesData;
             this.sockets = [];
-            this.data.map((socket) => this.sockets.push(socket["deviceName"]));
+            this.devicesData.map((socket) => this.sockets.push(socket["deviceName"]));
         })
         .catch(error => {
-            console.error(error); // if there's an error, it will be logged to the console
+            throw error; // if there's an error, it will be logged to the console
         });
+    }
+
+    fetchHouseSettings(){
+        var url = "http://192.168.2.145:8099/getInfo?";
+        var params = {
+            table : "HouseSettings",
+            keyName : "houseID",
+            keyValue : "H1"
+        };
+        params = new URLSearchParams(params);
+
+        fetch(url + params)
+        .then((response) => response.json())
+        .then((houseData) => {
+            this.houseData = houseData[0];
+        })
+        .catch(error => {
+            throw error; // if there's an error, it will be logged to the console
+        });
+    }
+
+    fetchData(){ // Fetch sockets settings
+        try{
+            this.fetchSocketSettings();
+            this.fetchHouseSettings();
+        } catch(e){
+            alert("An error occurred while fetching settings: \n\t" + e.message);
+        }
     }
 
     connectedCallback() {
         super.connectedCallback();
-        this.data = false;
-        this.loadSettings = false;
+        this.devicesData = false;
+        this.houseData = false;
+        this.loadSocketSettings = false;
+        this.loadHouseSettings = false;
         this.fetchData();
     }
 
     render() {
-        if(!this.data){
+        if(!this.devicesData | !this.houseData){
             return html`
                 <div> Loading... </div>
             `;
@@ -209,20 +307,22 @@ class MainPage extends LitElement {
             return html`
                 <app-header fixed="" slot="header"> </app-header>
                 
-                <div class="container" id="container" @err_occ=${this.errorHandler}>
+                <div class="container" id="container" @err_occ=${this.errorHandler} @click=${this.resetSavedState}>
                     
                     <div id="main-page" class="main-page" @click="${this.loadHACards}">
                         ${this.sockets.map((item) => html`<div class="child">
-                            <ha-card outlined class="socket_button" @click="${() => this.show_settings(item)}">
+                            <ha-card outlined class="socket_button" @click="${() => this.show_socket_settings(item)}">
                                 <div name="button_text" class="button_text">${item}</div>
                             </ha-card>
                         </div>`)}
                     </div>
 
-                    ${this.loadSettings ? html`<socket-settings id="socket-settings" class="socket-settings" .hass=${this.hass} .extData=${this.extData}></socket-settings>` : ""}
+                    ${this.loadSocketSettings ? html`<socket-settings id="socket-settings" class="settings" .hass=${this.hass} .extData=${this.extDeviceData}></socket-settings>` : ""}
                     
+                    ${this.loadHouseSettings ? html`<house-settings id="house-settings" class="settings" .hass=${this.hass} .extData=${this.houseData}></house-settings>` : ""}
+
                     <div class="btn_cont" id="btn_cont">
-                        <mwc-button class="back_btn" id="back_btn" @click=${this.hide_socket}>
+                        <mwc-button class="back_btn" id="back_btn" @click=${this.hide_settings}>
                             <ha-icon class="arrowLeft" id="arrowLeft" icon="mdi:arrow-left"></ha-icon>
                         </mwc-button>
                         <mwc-button class="save_btn" id="save_btn" @click=${this.save}>
@@ -230,6 +330,14 @@ class MainPage extends LitElement {
                             <div id="save_btn_text">Save</div>
                         </mwc-button>
                     </div>
+
+                    <div class="gen_settings_button" id="gen_settings_button" @click=${this.show_house_settings}>
+                        <div class="icon_dot" id="icon_dot">
+                            <ha-icon class="cog" id="cog" icon="mdi:cog"></ha-icon> 
+                        </div>
+                    </div>
+
+                    
                 </div>
             `;
         }
@@ -238,8 +346,6 @@ class MainPage extends LitElement {
     static get styles() {
         return css`
             .container{
-                width: 100%;
-                height: 100%;
                 float: left;
                 margin-top: 3px;
                 margin-left: 5px;
@@ -271,12 +377,8 @@ class MainPage extends LitElement {
                 justify-content: center;
             }
 
-            .socket-settings{
+            .settings{
                 display: none;
-                width: 100%;
-                justify-content: center;
-                align-content: flex-start;
-                flex-wrap: wrap;
                 padding-top: 56px; //var(--pd-top);
             }
 
@@ -293,6 +395,23 @@ class MainPage extends LitElement {
             .okTick{
                 display: none;
                 padding-right: 5px;
+            }
+
+            .gen_settings_button{
+                position: fixed;
+                bottom: 20px;
+                right: 20px;
+            }
+
+            .icon_dot{
+                background-color: var(--primary-color);
+                aspect-ratio: 1;
+                width: 40px;
+                border-radius: 50%;
+                display: flex;
+                flex-wrap: wrap;
+                align-content: center;
+                justify-content: center;
             }
         `;
     }
